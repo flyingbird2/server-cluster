373453f8e3df4bb3640d0e600a738872fb12d8d7
flyingbird2 <buhui.08@163.com>
2020-04-19 10:20:25 +0800

    test

--- src/main/java/com/example/servercluster/ServerClusterApplication.java
+++ src/main/java/com/example/servercluster/ServerClusterApplication.java
@@ -3,9 +3,11 @@ package com.example.servercluster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAspectJAutoProxy//
 public class ServerClusterApplication {
 
 	public static void main(String[] args) {
