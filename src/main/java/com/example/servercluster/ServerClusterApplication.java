2f73ac41957cdac43acf6b5cca55b7685c58bfa1
flyingbird2 <buhui.08@163.com>
2020-04-19 10:21:20 +0800

    local

--- src/main/java/com/example/servercluster/ServerClusterApplication.java
+++ src/main/java/com/example/servercluster/ServerClusterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAspectJAutoProxy//
+@EnableAspectJAutoProxy//local
 public class ServerClusterApplication {
 
 	public static void main(String[] args) {
